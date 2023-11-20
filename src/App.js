92b843700b067d75ae26c316ca44194b4f30fdd1
Elías <elias.ludviksson@gmail.com>
2023-11-20 21:56:10 +0000

    fikta-med-utlit

--- src/App.js
+++ src/App.js
@@ -26,36 +26,38 @@ export default function App() {
 		<div className='site-container'>
 			<Router>
 				<header>
-					<nav className='grid-container'>
-						<h1>
-							<Link to="/">Vefforitunarbúðin</Link>
-						</h1>
-						<menu>
-							<li>
-								<Link to="/nyskra">Nýskrá</Link>
-							</li>
-							<li>
-								<Link to="/innskra">Innskrá</Link>
-							</li>
-							<li>
-								<Link to="/karfa">Karfa</Link>
-							</li>
-						</menu>
-						<menu>
-							<li>
-								<Link to="/vara">Nýjar vörur</Link>
-							</li>
-							<li>
-								<Link to="/flokkar">Flokkar</Link>
-							</li>
-						</menu>
-					</nav>
+					<div className='rows'>
+						<nav className='grid-container'>
+							<h1>
+								<Link to="/">Vefforitunarbúðin</Link>
+							</h1>
+							<menu>
+								<li>
+									<Link to="/nyskra">Nýskrá</Link>
+								</li>
+								<li>
+									<Link to="/innskra">Innskrá</Link>
+								</li>
+								<li>
+									<Link to="/karfa">Karfa</Link>
+								</li>
+							</menu>
+							<menu>
+								<li>
+									<Link to="/vara">Nýjar vörur</Link>
+								</li>
+								<li>
+									<Link to="/flokkur">Flokkar</Link>
+								</li>
+							</menu>
+						</nav>
+					</div>
 				</header>
 				<main>
 					<Routes>
-						<Route path="/vara" element={<Vorulisti />} />
-						<Route path="/nyskra" element={<FlokkaSida />} />
-						<Route path="/innskra" element={<FlokkaSida />} />
+						<Route path="/" element={<Home />} />
+						<Route path="/flokkur" element={<Flokk teksti={'/categories'} />} />
+						{/* <Route path="/innskra" element={<FlokkaSida />} /> */}
 						<Route path="/flokkur/:id" element={<FlokkaSida />} />
 						<Route path="/vara" element={<Vorulisti />} />
 						<Route path="/vara/:id" element={<Vorusida />} />
@@ -65,6 +67,9 @@ export default function App() {
 		</div>
 	);
 }
+
+// Virkni til að fara á milli síða
+
 function OpenPL() {
 	window.history.pushState({}, '', '/vara')
 	window.history.go()
@@ -83,6 +88,8 @@ function OpenFlokk(inntak) {
 	window.history.go()
 }
 
+// Hlutar af síðum
+
 function ProdList(props) {
 	const sott = `${url}${props.teksti}`;
 	const { isLoading, error, data } = useFetch(sott);
@@ -94,6 +101,7 @@ function ProdList(props) {
 			onClick={OpenProduct} className='voruspjald'>
 			<figure>
 				<img src={vara.image ? vara.image : ''}
+					className='vorumynd'
 					alt={'mynd af vöru'}></img>
 				<figcaption className='voru-data'>
 					<h4 className='heiti'>{vara.title ? vara.title : `titill ${v}`}</h4>
@@ -104,7 +112,7 @@ function ProdList(props) {
 
 		</li >)
 	)
-	return (<ul>{arrayOfLists}</ul>);
+	return (<ul className='grid-container'>{arrayOfLists}</ul>);
 }
 
 function Product(props) {
@@ -113,32 +121,42 @@ function Product(props) {
 	if (isLoading) return 'Loading...';
 	if (error) return 'Error!';
 	const v = 'vantar';
-	return <figure>
+	return <figure className='grid-container'>
 		<img src={data.image ? data.image : ''}
+			className='adal-mynd'
 			alt={'mynd af vöru'}></img>
-		<h2 className='heiti'>{data.title ? data.title : `titill ${v}`}</h2>
-		<p className='flokk'>{data.category_title ? `Flokkur:${data.category_title}` : `flokk ${v}`}</p>
-		<p className='verd'>{data.price ? `Verð:${formatPrice(data.price)}` : `verð ${v}`}</p>
-		<p className='voru-lysing '>{data.description ? data.description : `vöru lýsinug ${v}`}</p>
+		<div className='adal-mynd'>
+			<h2 className='heiti'>{data.title ? data.title : `titill ${v}`}</h2>
+			<p className='flokk'>{data.category_title ? `Flokkur:${data.category_title}` : `flokk ${v}`}</p>
+			<p className='verd'>{data.price ? `Verð:${formatPrice(data.price)}` : `verð ${v}`}</p>
+			<p className='voru-lysing '>{data.description ? data.description : `vöru lýsinug ${v}`}</p>
+		</div>
 	</figure>
 }
+
+// Síður
 
 function Flokk(props) {
 	const ne = 'https://vef1-2023-h2-api-791d754dda5b.herokuapp.com'
 	const sott = `${ne}${props.teksti}`;
-	// const ne = "https://vef1-2023-h2-api-791d754dda5b.herokuapp.com"
 	const { isLoading, error, data } = useFetch(sott);
 	if (isLoading) return 'Loading...';
 	if (error) return 'Error!';
 	const v = 'vantar';
 	const arrayOfLists = Array.from(data.items).map(
 		vara => (<li key={vara.id ? vara.id : ''} id={vara.id} title={vara.title}
+			className='Flokkur'
 			onClick={OpenFlokk}>
 			<h4 className='flokk'>{vara.title ? vara.title : `flokk ${v}`}</h4>
 		</li >)
 	)
-	return (<ul>{arrayOfLists}</ul>);
+	return (<div>
+		<h3>Skoðaðu vöruflokkana okkar</h3>
+		<ul className='grid-container'>{arrayOfLists}</ul>
+	</div>);
 }
+
+
 
 function Vorusida() {
 	const { id } = useParams()
@@ -146,9 +164,9 @@ function Vorusida() {
 	const { isLoading, error, data } = useFetch(sott);
 	if (isLoading) return 'Loading...';
 	if (error) return 'Error!';
-	return <div>
+	return <div className='rows'>
 		<Product teksti={`/${id}`} />
-		<h3>Fleiri vörur úr {data.category_title ? data.category_title : 'sama flokki'}</h3>
+		<h3>Fleiri vörur úr flokki: {data.category_title ? data.category_title : 'sama flokki'}</h3>
 		<ProdList teksti={`?limit=3&category=${data.category_id}`} />
 		<h3>Aðrar vörur frá okkur</h3>
 		<ProdList teksti={'?limit=3'} />
@@ -157,11 +175,10 @@ function Vorusida() {
 
 function Home() {
 	return (
-		<div>
+		<div className='rows'>
 			<h2>Nýjar vörur</h2>
 			<ProdList teksti={'?limit=6'} />
 			<button onClick={OpenPL}>Skoða alla flokka</button>
-			<h3>Skoðaðu vöruflokkana okkar</h3>
 			<Flokk teksti={'/categories'} />
 		</div>);
 }
@@ -173,7 +190,7 @@ function FlokkaSida() {
 	if (isLoading) return 'Loading...';
 	if (error) return 'Error!';
 	return <div>
-		<h3>Skoðaðu vöruflokkana okkar</h3>
+		<h3>Flokkur: {data.category_title}</h3>
 		<ProdList teksti={`?limit=3&category=${id}`} />
 	</div>;
 }
@@ -183,6 +200,10 @@ function Vorulisti() {
 		<div>
 			<h2>Nýjar vörur</h2>
 			<ProdList teksti={'?limit=20'} />
+			<button onClick={(() => {
+				window.history.pushState({}, '', '/');
+				window.history.go();
+			})}>Til baka á forsíðu</button>
 		</div>
 	)
 }
