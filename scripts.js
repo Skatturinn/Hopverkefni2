6e1cc406d590624fdbf65af8d8ee0de1afd21f2c
SigurdurThorJ <sigurdurtj@gmail.com>
2023-11-23 10:02:32 +0000

    stytti javascript kóðan

--- scripts.js
+++ scripts.js
@@ -1,276 +1,127 @@
-function createProductElement(product) {
-  const productDiv = document.createElement('div');
-  productDiv.className = 'product';
-  productDiv.innerHTML = `
-      <a href="product.html?id=${product.id}">
+function createElement(tag, attributes, ...children) {
+    const element = document.createElement(tag);
+    for (const key in attributes) {
+      element[key] = attributes[key];
+    }
+    children.forEach(child => {
+      if (typeof child === 'string') {
+        element.innerHTML += child;
+      } else {
+        element.appendChild(child);
+      }
+    });
+    return element;
+  }
+  
+  function createProductElement(product) {
+    return createElement('div', { className: 'product' },
+      `<a href="product.html?id=${product.id}">
           <img src="${product.image}" alt="${product.title}">
           <h3>${product.title}</h3>
           <p>${product.price} kr.</p>
           <p>${product.category_title}</p>
-      </a>
-  `;
-  return productDiv;
-}
-
-function loadCategories() {
-  const apiURL = 'https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/categories';
-  fetch(apiURL)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`Error ${response.statusText}`);
-          }
-          return response.json();
-      })
-      .then(data => {
-          console.log('API response:', data);
-
-          const categoriesArray = data.items || data;
-          if (!Array.isArray(categoriesArray)) {
-              throw new Error('Expected an array of categories');
-          }
-
-          const categoriesContainer = document.getElementById('categories');
-          categoriesArray.forEach(category => {
-              const categoryDiv = document.createElement('div');
-              categoryDiv.className = 'category';
-              categoryDiv.innerHTML = `<h3>${category.title}</h3>`;
-
-              categoryDiv.onclick = function() {
-                  window.location.href = `products.html?category=${category.id}`;
-              };
-
-              categoriesContainer.appendChild(categoryDiv);
-          });
-      })
-      .catch(error => {
-          console.error('Error', error);
-      });
-}
-
-function loadProductsByCategory(categoryId) {
-  const apiURL = `https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?category=${categoryId}`;
-  fetch(apiURL)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`Error ${response.statusText}`);
-          }
-          return response.json();
-      })
-      .then(data => {
-          const productsArray = data.items || data;
-          const productsContainer = document.getElementById('products');
-
-          productsContainer.innerHTML = '';
-
-          productsArray.forEach(product => {
-              const productDiv = document.createElement('div');
-              productDiv.className = 'product';
-              productDiv.innerHTML = `
-                  <img src="${product.image}" alt="${product.title}">
-                  <h3>${product.title}</h3>
-                  <p>${product.price}</p>
-                  <p>${product.category_title}</p>
-              `;
-              productsContainer.appendChild(productDiv);
-          });
-      })
-      .catch(error => {
-          console.error('Error', error);
-      });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const categoryId = urlParams.get('category');
-  if (categoryId) {
-      loadProductsByCategory(categoryId);
+      </a>`
+    );
   }
-});
-
-
-function loadNewProducts() {
-  const apiURL = 'https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=6';
-  fetch(apiURL)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Error ' + response.statusText);
-          }
-          return response.json();
-      })
-      .then(data => {
-          const products = data.items;
-          const productsContainer = document.getElementById('new-products');
-          products.forEach(product => {
-              const productDiv = createProductElement(product);
-              productsContainer.appendChild(productDiv);
-              productDiv.onclick = function() {
-                  window.location.href = `product.html?id=${product.id}`;
-              };
-
-              productsContainer.appendChild(productDiv);
-          });
-      })
-      .catch(error => {
-          console.error('Error', error);
-      });
-}
-
-/*vörulisti*/
-function loadProductList() {
-  const apiURL = 'https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products';
-  fetch(apiURL)
-      .then(response => response.json())
-          .then(data => {
-              const productListContainer = document.getElementById('product-list');
-              productListContainer.innerHTML = '';
-
-              data.items.forEach(product => {
-                productListContainer.innerHTML += `
-                    <img src="${product.image}" alt="${product.title}">
-                    <h3>${product.title}</h3>
-                    <p>${product.price}</p>
-                    <p>${product.category_title}</p>
-                `;
-            });
-
-      })
-      .catch(error => {
-          console.error('Error', error);
-      });
-}
-
-
-/*vörusíða*/
-function loadProductDetails(productId) {
   
-
-  const apiURL = `https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products/${productId}`;
-  fetch(apiURL)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`Error ${response.status}`);
-          }
-          return response.json();
-      })
-      .then(product => {
-          const productDetailsContainer = document.getElementById('product-details');
-          if (!productDetailsContainer) {
-              console.error('vara fannst ekki');
-              return;
-          }
-          productDetailsContainer.innerHTML = `
-              <img src="${product.image}" alt="${product.title}">
-              <h3>${product.title}</h3>
-              <p>${product.price}</p>
-              <p>${product.category_title}</p>
-              <p>${product.description}</p>
-          `;
-
-          if (product.category_id) {
-              loadRelatedProducts(product.category_id);
-          }
-      })
-      .catch(error => {
-          console.error('Error', error);
-      });
-
-      if (product.category_id) {
-        loadRelatedProducts(product.category_id);
-      }
-
-  }
-
-
-  function loadRelatedProducts(categoryId) {
-    const apiURL = `https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=3&category=${categoryId}`;
-  
+  function loadDataFromAPI(apiURL, processFunction) {
     fetch(apiURL)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Error ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        const products = Array.isArray(data) ? data : data.items;
-        if (!Array.isArray(products)) {
-          throw new Error('Error');
-        }
-  
-        const productsContainer = document.getElementById('related-products'); 
-        productsContainer.innerHTML = ''; 
-  
-        products.forEach(product => {
-          const productDiv = document.createElement('div');
-          productDiv.className = 'product';
-          productDiv.innerHTML = `
-              <img src="${product.image}" alt="${product.title}">
-              <h3>${product.title}</h3>
-              <p>${product.price}</p>
-              <p>${product.category_title}</p>
-              <p>${product.description}</p>
-          `;
-          productsContainer.appendChild(productDiv);
-        });
-      })
-      .catch(error => {
-        console.error('Error', error);
-      });
+      .then(response => response.json())
+      .then(data => processFunction(data))
+      .catch(error => console.error('Error', error));
   }
 
-
-function getQueryParam(param) {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(param);
-}
-
-function createHeader() {
-  const header = el('header', { class: 'main-header' },
-    el('h1', { class: 'logo' }, 'Vefforitunarbúðin'),
-    el('nav', { class: 'main-nav' },
-      el('ul', {},
-        el('li', {}, el('a', { href: '#new-products' }, 'Nýjar vörur')),
-        el('li', {}, el('a', { href: '#categories' }, 'Flokkar')),
+  function createHeader() {
+    const header = createElement('header', { className: 'main-header' },
+      createElement('h1', { className: 'logo' }, 'Vefforitunarbúðin'),
+      createElement('nav', { className: 'main-nav' },
+        createElement('ul', {},
+          createElement('li', {}, createElement('a', { href: '#new-products' }, 'Nýjar vörur')),
+          createElement('li', {}, createElement('a', { href: '#categories' }, 'Flokkar'))
+        )
       )
-    )
-  );
-
-  document.body.prepend(header);
-  document.querySelector('.logo').addEventListener('click', () => {
-    window.location.href = '/';
-  });
-}
-
-function el(tag, attrs, ...children) {
-  const element = document.createElement(tag);
-  Object.entries(attrs).forEach(([key, value]) => {
-    element.setAttribute(key, value);
-  });
-  children.forEach(child => {
-    if (typeof child === 'string') {
-      element.appendChild(document.createTextNode(child));
-    } else if (child instanceof Node) {
-      element.appendChild(child);
-    }
-  });
-  return element;
-}
-
-function determinePage() {
-  const productId = getQueryParam('id');
-  createHeader();
-
-  if (window.location.pathname.includes('vorulisti.html')) {
-      loadProductList();
-  } else if (window.location.pathname.includes('product.html') && productId) {
-      loadProductDetails(productId);
-      loadRelatedProducts(productId);
-  } else if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
-      loadNewProducts();
-      loadCategories();
-  } else if (window.location.pathname.includes('products.html') && categoryId) {
-      loadProductsByCategory(categoryId);
+    );
+    document.body.prepend(header);
+    document.querySelector('.logo').addEventListener('click', () => {
+      window.location.href = '/';
+    });
   }
-}
 
-document.addEventListener('DOMContentLoaded', determinePage);
+  function loadCategories() {
+    loadDataFromAPI('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/categories', data => {
+      const categoriesContainer = document.getElementById('categories');
+      data.items.forEach(category => {
+        const categoryDiv = createElement('div', { className: 'category' },
+          `<h3>${category.title}</h3>`
+        );
+        categoryDiv.onclick = () => window.location.href = `products.html?category=${category.id}`;
+        categoriesContainer.appendChild(categoryDiv);
+      });
+    });
+  }
+  
+  function loadNewProducts() {
+    loadDataFromAPI('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=6', data => {
+      const productsContainer = document.getElementById('new-products');
+      data.items.forEach(product => productsContainer.appendChild(createProductElement(product)));
+    });
+  }
+  
+  function loadProductList() {
+    loadDataFromAPI('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products', data => {
+      const productListContainer = document.getElementById('product-list');
+      data.items.forEach(product => productListContainer.appendChild(createProductElement(product)));
+    });
+  }
+  
+  function loadProductDetails(productId) {
+    loadDataFromAPI(`https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products/${productId}`, product => {
+      const productDetailsContainer = document.getElementById('product-details');
+      productDetailsContainer.innerHTML = `
+          <img src="${product.image}" alt="${product.title}">
+          <h3>${product.title}</h3>
+          <p>${product.price} kr.</p>
+          <p>${product.category_title}</p>
+          <p>${product.description}</p>
+      `;
+      loadRelatedProducts(product.category_id);
+    });
+  }
+  
+  function loadRelatedProducts(categoryId) {
+    loadDataFromAPI(`https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=3&category=${categoryId}`, data => {
+      const relatedProductsContainer = document.getElementById('related-products');
+      data.items.forEach(product => relatedProductsContainer.appendChild(createProductElement(product)));
+    });
+  }
+  
+  function initializePage() {
+    createHeader();
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+    const categoryId = urlParams.get('category');
+  
+    if (window.location.pathname.includes('product.html') && productId) {
+      loadProductDetails(productId);
+    } else if (window.location.pathname.includes('products.html') && categoryId) {
+      loadProductsByCategory(categoryId);
+    } else if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
+      loadNewProducts();
+      loadCategories()
+    } else if (window.location.pathname.includes('vorulisti.html')) {
+      loadProductList();
+    }
+  }
+
+  function loadProductsByCategory(categoryId) {
+    const apiURL = `https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?category=${categoryId}`;
+    loadDataFromAPI(apiURL, data => {
+      const productsContainer = document.getElementById('products');
+      productsContainer.innerHTML = '';
+      data.items.forEach(product => {
+        productsContainer.appendChild(createProductElement(product));
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', initializePage);
