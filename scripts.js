41fd7f4e7c1af49a440cc953d5b605f414bed872
Elías <elias.ludviksson@gmail.com>
2023-11-25 05:37:50 +0000

    ff

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ function initializePage() {
 	const categoryTitle = urlParams.get('title');
 	const skoda = urlParams.get('skoda');
 	setLoading(mainEl)
-	const midja = createElement('div', { className: 'neðri' })
+	const midja = createElement('div', { className: 'nedri' })
 	if (productId) {
 		loadProductDetails(efriHluti, productId);
 		nedriHluti.appendChild(createElement('h2', {}, `Meira úr flokknum ${String(categoryTitle || 'vantar nafn flokks')}`))
